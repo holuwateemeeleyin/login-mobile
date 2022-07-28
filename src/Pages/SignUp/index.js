1800b286d2aba59d276605efd7e7d5a2816b9c3f
unknown <abegundeosamuel@gmail.com>
2022-07-28 23:01:27 +0100

    SignUp Page

---
+++ src/Pages/SignUp/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <div className='signup-container'>
+        <h3> Sign Up</h3>
+        <p>Please fill in the details and create account</p>
+        <form>
+          <label>Full Name</label>
+          <input type='text'/>
+          <label>Full Name</label>
+          <input type='text'/>
+        </form>
+    </div>
+  )
+}
