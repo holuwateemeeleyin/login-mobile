6fcf98bbf3a27fa9d0ff1a0dc3de669804029659
unknown <abegundeosamuel@gmail.com>
2022-08-05 22:01:08 +0100

    Navigate route

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import pic from '../../Assets/mobile.png'
 import {Link} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
 export default function Home() {
+
+  const navigate = useNavigate()
   return (
     <div className='home'>
       <img src={pic} alt='home' width='60%'/>
       <div className='home-text'>
         <h4> Welcome </h4>
         <p> Create an account and access thousand of cool stuffs</p>
-        <button className='home-button'>
+        <button 
+          className='home-button'
+          onClick={()=> navigate('/signin')}
+        >
           Get Started
         </button>
         <div className='already'>Already have an account? <Link to='/signin'>Sign in</Link></div>
