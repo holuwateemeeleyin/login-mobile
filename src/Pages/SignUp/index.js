f73edd7e3b799ce040ddf45ba417dbf296c0994a
unknown <abegundeosamuel@gmail.com>
2022-08-06 11:43:01 +0100

    added icon for showing/hiding password

--- src/Pages/SignUp/index.js
+++ src/Pages/SignUp/index.js
@@ -1,24 +1,37 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { MdFacebook } from 'react-icons/md'
+import { FcGoogle } from 'react-icons/fc'
+import { BiShow } from 'react-icons/bi'
 
 export default function SignUp() {
   const userRef = useRef()
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [pwd, setPwd] = useState('')
+
+  const eye = <BiShow size='25'/>
+  // useEffect(()=>{
+  //   userRef.current.focus()
+  // })
+
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+    setName('')
+    setEmail('')
+    setPwd('')
+  }
   
-  useEffect(()=>{
-    userRef.current.focus()
-  })
   return (
     <div className='signup-container'>
       <h3> Sign Up</h3>
       <p>Please fill in the details and create account</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className='form-input'>
           <label>Full Name</label>
           <input
-            ref={userRef} 
+            // ref={userRef} 
             type='text'
             value={name}
             onChange={(e)=> setName(e.target.value)}
@@ -26,7 +39,7 @@ export default function SignUp() {
         </div>
         <div className='form-input'>
           <label>Email Address</label>
-          <input 
+          <input
             type='email'
             value={email}
             onChange={(e)=> setEmail(e.target.value)}
@@ -34,18 +47,21 @@ export default function SignUp() {
         </div>
         <div className='form-input'>
           <label>Enter Password</label>
-          <input 
-            type='password'
-            value={pwd}
-            onChange={(e)=> setPwd(e.target.value)}
-          />
+          <div className='pass-wrapper'>
+            <input 
+              type='password'
+              value={pwd}
+              onChange={(e)=> setPwd(e.target.value)}
+            />
+            <i>{eye}</i>
+          </div>
         </div>
         <button className='button'>
           Create account
         </button>
         <div className='already'>Already have an account? <Link to='/signin'> Sign in</Link></div>
       </form>
-      <div className='continue'>_______________ or continue with ______________ </div>
+      
     </div>
   )
 }
