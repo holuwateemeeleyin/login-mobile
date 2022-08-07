c54fedf6fbbe5ffd9e18d6fc489ece3abd70500e
unknown <abegundeosamuel@gmail.com>
2022-08-07 17:58:40 +0100

    Added Loader

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Home from './Pages/Home';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import './App.css'
 import SignUp from './Pages/SignUp';
 import Signin from './Pages/SignIn';
-
+import { AnimatePresence } from 'framer-motion'
 
 function App() {
+  const location = useLocation()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      setLoading(false)
+    }, 2000)
+    return()=> clearTimeout(timer)
+  }, [])
+
+  if (loading){
+    return <div className='loading'></div>
+  }
+
   return (
-    <div className='app'>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/signin' element={<Signin/>}/>
-      </Routes>
-    </div>
+    <AnimatePresence>
+      <div className='app'>
+        <Routes location={location} key={location.pathname}>
+          <Route path='/' element={<Home/>} />
+          <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/signin' element={<Signin/>}/>
+        </Routes>
+      </div>
+    </AnimatePresence>
   );
 }
 
